662284816b4249a8fdda4a74653e4f99090caf4f
Gerardo Resendiz Vega <gerardoresendizvega@gmail.com>
2025-01-18 12:00:15 -0600

    switch-case clase 180125

---
+++ switch-case-clase.js
@@ -0,0 +1,24 @@
+
+let expression = 3;
+
+switch(expression) {
+    case 1:
+      console.log("segundo dia")
+      break;
+    case 2:
+        console.log("es 2")
+      break;
+    default:
+        console.log("es default")
+  }
+
+  let calificacion = "A";
+
+  switch(calificacion) {
+    case "A":
+      console.log("Exelente")
+      break;
+ 
+    default:
+        console.log("No Excelente")
+  }
