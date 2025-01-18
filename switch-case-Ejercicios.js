5eaeaccb3682d04d6f8455ce73af2f997a8f61c3
Gerardo Resendiz Vega <gerardoresendizvega@gmail.com>
2025-01-18 12:27:58 -0600

    Ejercicio meses del año

--- switch-case-Ejercicios.js
+++ switch-case-Ejercicios.js
@@ -63,5 +63,53 @@ switch(calificacion) {
 
   console.log("\n ----------Fin Ejercicio #2 ---------- \n");
 
+  console.log("----------Ejercicio #3 (Meses del año )---------- \n");
+
+  let mes = 5;
+switch (mes) {
+    case 1:
+        console.log("Enero");
+        break;
+    case 2:
+        console.log("Febrero");
+        break;
+    case 3:
+        console.log("Marzo");
+        break;
+    case 4:
+        console.log("Abril");
+        break;
+    case 5:
+        console.log("Mayo");
+        break;
+    case 6:
+        console.log("Junio");
+        break;
+    case 7:
+        console.log("Julio");
+        break;
+    case 8:
+        console.log("Agosto");
+        break;
+    case 9:
+        console.log("Septiembre");
+        break;
+    case 10:
+        console.log("Octubre");
+        break;
+    case 11:
+        console.log("Noviembre");
+        break;
+    case 12:
+        console.log("Diciembre");
+        break;
+    default:
+        console.log("Número inválido");
+}
+
+
+  console.log("\n ----------Fin Ejercicio #3 ---------- \n");
+
+
 
   
