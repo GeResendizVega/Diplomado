7f230d012b64a3f628352421c5bc31ad5985fc33
Gerardo Resendiz Vega <gerardoresendizvega@gmail.com>
2025-01-18 12:07:48 -0600

    Creacion de gitignore

---
+++ switch-case-Ejercicios.js
@@ -0,0 +1,27 @@
+let diaSemana = 3;
+
+switch(diaSemana) {
+    case 1:
+      console.log("Lunes")
+      break;
+    case 2:
+        console.log("Martes")
+      break;
+      case 3:
+        console.log("Miercoles")
+      break;
+      case 4:
+        console.log("Jueves")
+      break;
+      case 5:
+        console.log("Viernes")
+      break;
+      case 6:
+        console.log("Ssabado")
+      break;
+      case 7:
+        console.log("Domingo")
+      break;
+    default:
+        console.log("No hay dia de la semana ")
+  }
