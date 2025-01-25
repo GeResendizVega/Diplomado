6e2f415d12cbdb8fa3ffcdaaad8baef831c066e0
Gerardo Resendiz Vega <gerardoresendizvega@gmail.com>
2025-01-25 09:51:53 -0600

    ejercicio fruta

--- switch-case-clase.js
+++ switch-case-clase.js
@@ -22,3 +22,25 @@ switch(expression) {
     default:
         console.log("No Excelente")
   }
+
+
+
+
+
+
+
+  let fruta = 'Naranja';
+switch (fruta) {
+      case 'Manzana':
+      case 'Pera':
+      case 'Platano':
+        console.log(fruta +" es de color Amarillo o verde");
+        break;
+      case 'Fresa':
+      case 'Mandarina':
+      case 'Naranja':
+        console.log(fruta +" es de color Rojo o Anaranjado");
+        break;
+    default:
+        console.log("Fruta no encontrada");
+}
