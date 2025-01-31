819442558d291f70fb18e4d4e177875960023a7a
Gerardo Resendiz Vega <gerardoresendizvega@gmail.com>
2025-01-31 11:54:26 -0600

    ejercicio 1 y 2 plataforma

--- bucle-for-ejercicios.js
+++ bucle-for-ejercicios.js
@@ -56,16 +56,3 @@ for (let i = 1; i < peliculas.length ; i++){
 
 
 
-    /*************************************************************************************/
-    /***************ejercicios de la plataforma */
-
-    /**
-
-    EJERCICIO  #1: Imprimir números del 1 al 10
-    Escribe un programa que imprima los números del 1 al 10.
-    */
-
-
-    for (let i = 1; i <= 10; i++) {
-        console.log(i);
-    }
